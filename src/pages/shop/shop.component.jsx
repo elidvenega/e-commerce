e5301ecaf8227072718c791282ad8cd976476625
elid venega <venegaelid@gmail.com>
2020-11-29 22:45:32 -0800

    redux part finish

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.jsx'
-import CollectionPreview from '../../components/collection-preview/collection-preview.jsx';
 
-class ShopPage extends React.Component {
-        constructor(props) {
-                super(props);
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-                this.state = {
-                    collections: SHOP_DATA 
-                            
-                        }
-                    }
-                    render() {
-                        const {collections} = this.state;
-                        return (<div className="shop-page">
-                            {
-                                collections.map(({id, ...otherCollectionProps}) => (
-                                    <CollectionPreview key={id} {...otherCollectionProps} />
-                                ))
-                            }
-                        </div>);
-                    }
-                }
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+       <CollectionsOverview />
+    </div>
+);
 
 export default ShopPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
